3e1177c43a061ba89e924c0db6e6548d85b3e609
amitsharma <55508548+amit17133129@users.noreply.github.com>
2021-08-25 18:49:00 +0530

    Create vault_variable.tf

---
+++ vault/vault_variable.tf
@@ -0,0 +1,13 @@
+variable "vault_addr" {
+ default = "http://public_ip:8200"
+}
+variable "vault_token" {
+ default = "xxxxxxxxxxxxxxxxxxxxx"
+}
+variable "access_key" {
+ default = "xxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "secret_key" {
+ default = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
